0ffb196e38b462681a2f0b0625b75d52bb12c849
chandrusuresh <chandrusuresh@gmail.com>
2018-06-24 09:28:57 -0400

    had some issues with initialization with the last commit with conv2d_3 layer. Trying something from the web

--- main.py
+++ main.py
@@ -50,28 +50,53 @@ tests.test_load_vgg(load_vgg, tf)
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
-    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
     :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
-    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
+    :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    layer7_1Dconv = tf.layers.conv2d(vgg_layer7_out, num_classes,1,padding='same',kernel_initializer= tf.random_normal_initializer(stddev=0.01),kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    conv7_2x = tf.layers.conv2d_transpose(layer7_1Dconv, num_classes, 4, strides=(2,2), padding ='same',kernel_initializer= tf.random_normal_initializer(stddev=0.01),kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-
-    layer4_1Dconv = tf.layers.conv2d(vgg_layer4_out, num_classes,1,padding='same',kernel_initializer= tf.random_normal_initializer(stddev=0.01),kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    skipLayer1 = tf.add(conv7_2x, layer4_1Dconv)
-    skipLayer1_2x = tf.layers.conv2d_transpose(skipLayer1, num_classes, 4, strides=(2,2), padding ='same',kernel_initializer= tf.random_normal_initializer(stddev=0.01),kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-
-    layer3_1Dconv = tf.layers.conv2d(vgg_layer3_out, num_classes,1,padding='same',kernel_initializer= tf.random_normal_initializer(stddev=0.01),kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    skipLayer2 = tf.add(skipLayer1_2x, layer3_1Dconv)
-
-    final_layer = tf.layers.conv2d_transpose(skipLayer2, num_classes, 16, strides=(8,8), padding ='same',kernel_initializer= tf.random_normal_initializer(stddev=0.01),kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    return final_layer
-
+    # 1x1 convolution of vgg layer 7
+    layer7a_out = tf.layers.conv2d(vgg_layer7_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    # upsample
+    layer4a_in1 = tf.layers.conv2d_transpose(layer7a_out, num_classes, 4,
+                                             strides= (2, 2),
+                                             padding= 'same',
+                                             kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                             kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    # make sure the shapes are the same!
+    # 1x1 convolution of vgg layer 4
+    layer4a_in2 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    # skip connection (element-wise addition)
+    layer4a_out = tf.add(layer4a_in1, layer4a_in2)
+    # upsample
+    layer3a_in1 = tf.layers.conv2d_transpose(layer4a_out, num_classes, 4,
+                                             strides= (2, 2),
+                                             padding= 'same',
+                                             kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                             kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    # 1x1 convolution of vgg layer 3
+    layer3a_in2 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    # skip connection (element-wise addition)
+    layer3a_out = tf.add(layer3a_in1, layer3a_in2)
+    # upsample
+    nn_last_layer = tf.layers.conv2d_transpose(layer3a_out, num_classes, 16,
+                                               strides= (8, 8),
+                                               padding= 'same',
+                                               kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                               kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    return nn_last_layer
 tests.test_layers(layers)
-
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
